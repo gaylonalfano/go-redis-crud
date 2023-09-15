e0e1cf64181b625c8c4c705697d8c2219a0ae71e
gaylonalfano <gaylon.alfano@gmail.com>
2023-09-15 16:41:43 +0300

    Basic server up and going

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	// Storing 'server' as a pointer, which means we're storing the memory
+	// address, NOT as a value!
+	server := &http.Server{
+		Addr:    ":3000",
+		Handler: http.HandlerFunc(basicHandler),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("Failed to listen to server", err)
+	}
+}
+
+// 'r' is a pointer of type http.Request (the inbound HTTP request from client)
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	// NOTE: We could impl our own basic router like below, but this
+	// is complicated handling path params, etc.
+	// if r.Method == http.MethodGet {
+	// 	// Handle GET
+	// 	if r.URL.Path == "/foo" {
+	// 		// Handle GET /foo
+	// 	}
+	// }
+	//
+	// if r.Method == http.MethodPost {
+	// 	// Handle POST
+	// }
+
+	w.Write([]byte("Hello, world!"))
+}
