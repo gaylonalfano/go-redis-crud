a77eeab82b728e15e562e68871fb271d34d3ba80
gaylonalfano <gaylon.alfano@gmail.com>
2023-09-16 07:49:50 +0300

    Chi router and middleware added for better logging

--- main.go
+++ main.go
@@ -5,10 +5,12 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
 	router := chi.NewRouter()
+	router.Use(middleware.Logger)
 	router.Get("/hello", basicHandler)
 
 	// Storing 'server' as a pointer, which means we're storing the memory
