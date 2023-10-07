39b97742ce8ef35c348cbb1a665e4ed85ebacb1b
gaylonalfano <gaylon.alfano@gmail.com>
2023-10-07 11:21:58 +0300

    Have working Update and Delete handlers. Pagination still broken

--- handler/order.go
+++ handler/order.go
@@ -153,8 +153,102 @@ func (h *Order) GetByID(w http.ResponseWriter, r *http.Request) {
 
 func (h *Order) UpdateByID(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Update an order by ID")
+	// 'body' to represent PUT data from client
+	var body struct {
+		Status string `json:"status"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// Pull out the Order ID
+	idParam := chi.URLParam(r, "id")
+
+	const base = 10
+	const bitSize = 64
+
+	orderID, err := strconv.ParseUint(idParam, base, bitSize)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// Retrieve existing order
+	currentOrder, err := h.Repo.FindByID(r.Context(), orderID)
+	if errors.Is(err, order.ErrNotExist) {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	} else if err != nil {
+		fmt.Println("Failed to find by id:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Only allow updating Order if certain conditions met
+	const completedStatus = "completed"
+	const shippedStatus = "shipped"
+	now := time.Now().UTC()
+
+	switch body.Status {
+	case shippedStatus:
+		if currentOrder.ShippedAt != nil {
+			// TODO: Send by custom error messages to client
+			fmt.Println("Failed to update status to:", shippedStatus)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		currentOrder.ShippedAt = &now
+	case completedStatus:
+		if currentOrder.CompletedAt != nil || currentOrder.ShippedAt == nil {
+			fmt.Println("Failed to update status to:", completedStatus)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		currentOrder.CompletedAt = &now
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	err = h.Repo.Update(r.Context(), currentOrder)
+	if err != nil {
+		fmt.Println("Failed to insert:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// If all is well, send it back to client encoded as JSON
+	if err := json.NewEncoder(w).Encode(currentOrder); err != nil {
+		fmt.Println("Failed to marshal:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
 }
 
 func (h *Order) DeleteByID(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Delete an order by ID")
+	idParam := chi.URLParam(r, "id")
+
+	const base = 10
+	const bitSize = 64
+
+	orderID, err := strconv.ParseUint(idParam, base, bitSize)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	err = h.Repo.DeleteByID(r.Context(), orderID)
+	if errors.Is(err, order.ErrNotExist) {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	} else if err != nil {
+		fmt.Println("Failed to find by id:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
 }
